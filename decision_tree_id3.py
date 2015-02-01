fb516549047b9ca4c5357bf49532cb7beb393844
yuande <miraclecome@gmail.com>
2015-02-01 22:01:51 +0800

    add entropy

--- decision_tree_id3.py
+++ decision_tree_id3.py
@@ -5,5 +5,19 @@
 from __future__ import print_function
 from __future__ import division
 
+from collections import Counter
 import numpy as np
 
+def source_entropy(Y):
+    m, _ = np.shape(Y)
+    labels = Y.T.A[0]
+
+    entropy = 0
+    frequency = Counter()
+    for value in set(labels):
+        frequency[value] = np.sum(labels == value) / m
+
+    for value, freq in frequency.iteritems():
+        entropy -= freq * np.log2(freq)
+
+    return entropy
