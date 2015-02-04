4ade8d93e6ef51aff86660dd2d006bd295e4ab64
yuande <miraclecome@gmail.com>
2015-02-04 18:09:52 +0800

    fix

--- knn/knn.py
+++ knn/knn.py
@@ -13,7 +13,7 @@ from __future__ import division
 from collections import Counter
 
 import os, sys
-parent_dir = os.path.abspath(os.path.dirname(__file__) + '..')
+parent_dir = os.path.abspath(os.path.dirname(__file__) + './../')
 sys.path.append(parent_dir)
 import toolkit
 
