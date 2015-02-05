2be22005a0a9038e4b543ada353c8ea86b7c357a
yuande <miraclecome@gmail.com>
2015-02-05 15:24:15 +0800

    fix some code

--- matrix.h
+++ matrix.h
@@ -1,3 +1,23 @@
+/*
+ * MachineLearning, open source software machine learning.
+ * Copyright (C) 2014-2015
+ * mailto:miraclecome AT gmail DOT com
+ *
+ * MachineLearning is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * MachineLearning is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #ifndef _MATRIX_H
 #define _MATRIX_H 1
 
@@ -14,11 +34,10 @@ template < class T, size_t row, size_t col >
 class Matrix
 {
 public:
-//    Matrix();
-//    virtual ~Matrix();
 
     Matrix() {}
     virtual ~Matrix() {}
+
     Matrix (T first, ...)
     :iRows(row), iCols(col) {}
 
