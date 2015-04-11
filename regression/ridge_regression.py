24cf73a2c30f38fe8ecc4a1aaa29b57716c322a9
yuande <miraclecome@gmail.com>
2015-04-12 00:53:26 +0800

    add cross validation

--- regression/ridge_regression.py
+++ regression/ridge_regression.py
@@ -61,16 +61,52 @@ def draw_parameters_trend(X, Y):
 def cross_validation(X, Y):
     """ To get lowest error and best alpha
     """
+    m, n = np.shape(X)
     ridge_weights, alphas = generate_parameters(X[:500], Y[:500])
     _, X_mean, X_std = feature_normalize(X[:500])
     X_test = (X[500:] - X_mean) / X_std
     for i, weight in enumerate(ridge_weights):
+# [:, np.newasix] change (3,) to (3,1)
         error = np.power(X_test * weight[:, np.newaxis] - Y[500:], 2).sum()
         print('alpha {}, test error is {}'.format(alphas[i], error))
+
+def cross_validation_10(X, Y, num=10):
+    """ 10折
+    """
+    m, n = np.shape(X)
+    errors = np.zeros((num, 60))
+    
+    index_list = range(m)
+    for i in range(num):
+        np.random.shuffle(index_list)
+        X_train, X_test = [], []
+        Y_train, Y_test = [], []
+        for j in range(m):
+            if j < 0.9 * m:
+                X_train.append(X[index_list[j]].A[0]) # (3,)
+                Y_train.append(Y[index_list[j]].A[0])
+            else:
+                X_test.append(X[index_list[j]].A[0])
+                Y_test.append(Y[index_list[j]].A[0])
+        ridge_weights, alphas = generate_parameters(np.mat(X_train), np.mat(Y_train))
+        X_normalize, X_mean, X_std = feature_normalize(np.mat(X_train))
+        for k in range(len(ridge_weights)):
+            _X = (np.mat(X_test) - X_mean) / X_std
+            errors[i, k] = np.power(_X * ridge_weights[k][:, np.newaxis] - np.mat(Y_test), 2).sum()
+    mean_error_of_alpha = np.mean(errors, 0)
+    min_error_of_alpha  = np.min(mean_error_of_alpha)
+    which_group_of_alpha = np.nonzero(mean_error_of_alpha == min_error_of_alpha)
+    # print(ridge_weights.shape, errors.shape) # (60, 3), (10, 60)
+
+    best_weight = ridge_weights[which_group_of_alpha]
+    best_alpha  = np.mat(alphas).T[which_group_of_alpha[0]]
+    print('best alpha {}, best weight {}'.format(best_alpha, best_weight))
+
 
 
 if __name__ == '__main__':
     data, label = load_data('linear_data_3d.txt')
     draw_parameters_trend(data, label)
     cross_validation(data, label)
+    cross_validation_10(data, label, num=10)
 
