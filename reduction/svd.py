e82af74850fadec0223d8ac931c05dcb6e57b897
laoliu <q_0_0_p@163.com>
2021-01-05 17:59:28 +0800

    add svd row compression

--- reduction/svd.py
+++ reduction/svd.py
@@ -9,6 +9,11 @@ import numpy.linalg as LA
 
 
 def svd(X, ratio=0.9):
+    """
+        行压缩
+        A * A^T 是描述样本数据间相关关系的矩阵，其正交基U是以样本空间进行展开的。
+        选取前r 个特征值来表示原始数据集：X_{r x n} = U^T_{r x m} * A_{m x n}
+    """
     U, Sigma, Vt = LA.svd(X) # m*m, ndarray, n*n
     idx = 0; proportion = 0
     for i, sigma in enumerate(np.power(Sigma, 2) / sum(np.power(Sigma, 2))):
